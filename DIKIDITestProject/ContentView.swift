e6d8d6153aa446256cfc788efb4699bad1ff2fbc
Andrei Panasenko <andrei_kmv@inbox.ru>
2022-12-13 11:08:15 +0300

    Initial Commit

---
+++ DIKIDITestProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DIKIDITestProject
+//
+//  Created by Andrei Panasenko on 13.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DIKIDITestProject/DIKIDITestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIKIDITestProjectApp.swift
+//  DIKIDITestProject
+//
+//  Created by Andrei Panasenko on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct DIKIDITestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
